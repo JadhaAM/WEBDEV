835e0605791fd90de5edfa1ec4f30263b3c77208
JadhaAM <134080298+JadhaAM@users.noreply.github.com>
2023-06-18 22:44:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+    let fullImgBox = document.getElementById("fullImgBox");
+    let fullImg = document.getElementById("fullImg");
+
+    function openFullImg(pic) {
+
+        fullImgBox.style.display = "flex";
+        fullImg.src = pic
+
+    }
+
+    function closeFullImg() {
+        fullImgBox.style.display = "none";
+    }
+
+    function full() {
+        document.getElementById('img-gallary').style.width = "25%";
+     
+    }
+
+    function twoFull() {
+        document.getElementById('img-gallary').style.width = "50%";
+        
+    }
+
+    function fourFull() {
+        document.getElementById('img-gallary').style.width = "100%";
+        
+    }
+
+// this for the preloder
+
+let loader = document.querySelector("#loader");
+
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+});
+

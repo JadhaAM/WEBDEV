830b4a6b8c2656daba06616e275cae34cfa38551
JadhaAM <134080298+JadhaAM@users.noreply.github.com>
2024-02-03 21:09:54 +0530

    day3
    basic of function in js

---
+++ Feb/loop1.js
@@ -0,0 +1,9 @@
+let j=10;
+for (let i = 10; i >0;) {
+    
+    if (j%2!=0) {
+        console.log(i-j);
+        i--;
+    }
+    j--;
+}
---
+++ Feb/spaceCount.js
@@ -0,0 +1,7 @@
+let string="rakesh is good boy"
+let count=0
+for (let ch in string) {
+      count++
+}
+
+console.log(count);

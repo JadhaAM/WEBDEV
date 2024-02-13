19c66348dab683d6444d80d1dfa581eb2a213c65
JadhaAM <134080298+JadhaAM@users.noreply.github.com>
2024-02-13 22:00:37 +0530

    flappy brid game
    adding user controlls

---
+++ Feb/day13/index.js
@@ -0,0 +1,82 @@
+class Game{
+    constructor(canvas,context){
+        this.canvas=canvas;
+        this.ctx=context;
+        this.width=this.canvas.width;
+        this.height=this.canvas.height;
+        this.baseHeight=720;
+        this.ratio=this.height/this.baseHeight;
+        this.player=new player(this);
+        this.gravity;
+
+         this.resize(window.innerWidth,window.innerHeight)
+        window.addEventListener("resize",e=>{
+            console.log(e);
+          this.resize(e.currentTarget.innerWidth ,e.currentTarget.innerHeight);
+        });
+
+        // control player using mouse
+
+        this.canvas.addEventListener("mousedown",e=>{
+            this.player.flap();
+        });
+
+        
+        // control player using keybord
+
+        window.addEventListener("keydown",e=>{
+            console.log(e.key);
+            if (e.key===" " || e.key==="Enter") {
+                this.player.flap();
+            }
+          
+        });
+
+        // control player using touch
+
+        this.canvas.addEventListener("touchstart",e=>{
+           
+            this.player.flap();
+        });
+
+
+    }
+    resize(width,height){
+        this.canvas.width=width;
+        this.canvas.height=height;
+        this.ctx.fillStyle = "red";
+        this.width=this.canvas.width;
+        this.height=this.canvas.height;
+        this.ratio=this.height/this.baseHeight;
+        this.gravity=0.20 * this.ratio;
+
+        this.player.resize();
+        
+    }
+
+    render(){
+        
+        this.player.update();
+        this.player.draw();
+    }
+}
+
+window.addEventListener("load",function(){
+
+    const canvas=document.querySelector("#contaner");
+    const ctx= canvas.getContext('2d');
+    canvas.width=720;
+    canvas.height=720;
+    
+
+
+ const game  = new Game(canvas,ctx);
+ game.render();
+
+ function animate() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    game.render();
+    requestAnimationFrame(animate);
+ }
+   requestAnimationFrame(animate);  
+})
---
+++ Feb/day13/player.js
@@ -0,0 +1,57 @@
+class player {
+    constructor(game) {
+        // Player initialization code
+        this.game=game;
+        this.x=20;
+        this.y;
+        this.spritWidth=200;
+        this.spritHeight=200;
+        this.width;
+        this.height;
+        this.speedY;
+        this.flapSpeed;
+    }
+
+    update() {
+        // Update player logic
+
+        this.y+=this.speedY;
+        if (!this.isBottom()) {
+            this.speedY+=this.game.gravity;
+        } 
+
+        //  bottom push 
+        if (this.isBottom()) {
+            this.y=this.game.height-this.height;;
+            
+        }
+    }
+
+    draw() {
+        // Draw player logic
+        this.game.ctx.fillRect(this.x,this.y,this.width,this.height);
+    }
+
+    resize(){
+        this.width=this.spritWidth *this.game.ratio;
+        this.height=this.spritHeight *this.game.ratio;
+        this.y=this.game.height*0.5-this.height-0.5;
+        this.speedY=-8 +this.game.ratio;
+        this.flapSpeed= 5 + this.game.ratio;
+    }
+
+    isTuchingTop(){
+        return this.y<=0;
+    }
+
+    isBottom(){
+        return this.y >=this.game.height-this.height;
+    }
+
+    flap(){
+        if (!this.isTuchingTop()) {
+            
+            this.speedY=-this.flapSpeed;
+        }
+    }
+}
